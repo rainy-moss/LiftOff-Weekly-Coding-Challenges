e7ba8d0dd08ddb66cd1f7cb4f8d6934ff3233e73
Rainy <112306653+rainy-moss@users.noreply.github.com>
2023-04-30 00:11:26 -0500

    completed week 3 coding challenge

---
+++ Week3_Coding_Challenge.js
@@ -0,0 +1,10 @@
+function FirstReverse(str) { 
+
+    str = str.split("").reverse().join("");
+  
+    return str; 
+  
+  }
+     
+  // keep this function call here 
+  console.log(FirstReverse(readline()));
